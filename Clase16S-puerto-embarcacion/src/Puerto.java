0040391cece8a44a31ebd6de001f540040b1caa1
FabricioDa <fabriciodanielturrina@gmail.com>
2022-09-05 23:05:35 -0300

    hola!

---
+++ Clase16S-puerto-embarcacion/src/Puerto.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Puerto {
+    private String nombre;
+    private ArrayList <Contenedor> contenedores;
+
+    public Puerto(String nombre) {
+        this.nombre = nombre;
+        this.contenedores = new ArrayList<>();
+    }
+    public void ingresarContenedor(Contenedor cont){
+
+    }
+    public int calcularCantidadContenedoresPeligrosos(ArrayList<Contenedor> contenedores){
+
+    }
+    public void ordenar(){
+
+       return contenedores.sort;
+}
+
+
+
+
+}
