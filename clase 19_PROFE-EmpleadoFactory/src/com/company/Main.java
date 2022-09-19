01da254b4099f290494a0bddfe3a591d8b381bcb
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2022-09-19 13:01:37 -0300

    hola

--- clase 19_PROFE-EmpleadoFactory/src/com/company/Main.java
+++ clase 19_PROFE-EmpleadoFactory/src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	Empresa empresa=new Empresa("DH");
-	EmpleadoFactory empleadoFactory= EmpleadoFactory.getInstancia(); // crea el objeto pero lo deja INTERNO para que yo le pida cosas
+	EmpleadoFactory empleadoFactory= EmpleadoFactory.getInstancia(); // crea el objeto pero lo deja INTERNO para que yo
+		// le pida cosas
+
 	empresa.agregarEmpleado(empleadoFactory.crearEmpleado(EmpleadoFactory.EMPLEADO_POR_HORA, "Juan", "Perez", 111));
 	empresa.agregarEmpleado(empleadoFactory.crearEmpleado(EmpleadoFactory.EMPLEADO_RELACION_DEPENDENCIA, "Maria", "Gomez", 222));
 	Empleado empleado=empleadoFactory.crearEmpleado("ERD","Juan", "Perez", 111);
